c496982ced72f62e7a3fa46ad3f1e76071e0a70f
Nickshakattack <nicholas_shaknes@avenues-ny.org>
2024-12-08 19:14:18 -0500

    made the balls and set up the canvas

--- sketch.js
+++ sketch.js
@@ -1,1 +1,17 @@
   
+
+let shapes = [];
+
+function setup() {
+  createCanvas(600, 400);
+  for (let i = 0; i < 10; i++) {
+    shapes.push({
+      x: random(width),
+      y: random(height),
+      speedX: random(2, 5),
+      speedY: random(2, 5),
+      size: random(20, 50),
+      color: color(random(255), random(255), random(255)),
+    });
+  }
+}
