c9eec59ce10d2ff51a5b39bef2a29f4eb43347a8
Nickshakattack <nicholas_shaknes@avenues-ny.org>
2024-12-08 19:15:29 -0500

    drew the balls and made the balls have random speeds

--- sketch.js
+++ sketch.js
@@ -15,3 +15,23 @@ function setup() {
     });
   }
 }
+
+
+function draw() {
+  background(220);
+
+ 
+  for (let i = 0; i < shapes.length; i++) {
+    let shape = shapes[i];
+
+    
+    shape.x += shape.speedX;
+    shape.y += shape.speedY;
+
+    
+    if (shape.x < 0 || shape.x > width) {
+      shape.speedX *= -1;  
+    }
+    if (shape.y < 0 || shape.y > height) {
+      shape.speedY *= -1; 
+    }
